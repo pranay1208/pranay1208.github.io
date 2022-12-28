fbd1f9d81781e1b454e135ce44591dad92e9265a
Pranay Agarwal <pranay12@connect.hku.hk>
2022-12-28 11:00:46 +0530

    Material UI is here

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Pranay Agarwal - Portfolio</p>
-        <a
-          className='App-link'
-          href='https://pranay.webflow.io'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Current Portfolio
-        </a>
-      </header>
-    </div>
+    <LandingPage />
+    // <div className='App'>
+    //   <header className='App-header'>
+    //     <img src={logo} className='App-logo' alt='logo' />
+    //     <p>Pranay Agarwal - Portfolio</p>
+    //     <a
+    //       className='App-link'
+    //       href='https://pranay.webflow.io'
+    //       target='_blank'
+    //       rel='noopener noreferrer'
+    //     >
+    //       Current Portfolio
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+const LandingPage = () => {
+  return <></>;
+};
+
+export default LandingPage;
